b637b485969675e787708455c79e714df089a1e6
김혜준 <alice5855@naver.com>
2023-01-12 16:41:24 +0900

    add NotFound h1

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,9 @@ import LogoImg from './곰.jpg'
 function NotFound() {
     return (
         <div>
+            <h1>찾을 수 없는 페이지입니다...!</h1>
             <img src={LogoImg} alt='logo-img'/>
+
         </div>
     );
 }
